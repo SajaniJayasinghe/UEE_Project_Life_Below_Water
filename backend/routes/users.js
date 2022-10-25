83259495ccc18341451ac2f7e5671e8d87eef6ce
Sajani Jayasinghe <jayasinghesajani98@gmail.com>
2022-10-25 09:51:55 +0530

    donation crud

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -8,6 +8,8 @@ const jwt = require("jsonwebtoken");
 const { RegisterUser } = require("../controllers/user.controller");
 const { LoginUser } = require("../controllers/user.controller");
 
+
+
 UserRouter.post("/registeruser", RegisterUser);
 UserRouter.post("/login", LoginUser);
 
